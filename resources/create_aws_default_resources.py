bde49d6146eeb904c22422849c7577585e667b25
Quentin Vallin <quentin.vallin@fxinnovation.com>
2020-08-17 08:20:16 -0400

    feat: improved error exception

--- resources/create_aws_default_resources.py
+++ resources/create_aws_default_resources.py
@@ -2,17 +2,32 @@
 
 import boto3
 from botocore.exceptions import ClientError
+from botocore.exceptions import ParamValidationError
 
 ec2 = boto3.client('ec2')
 for region in ec2.describe_regions()['Regions']:
   client = boto3.client('ec2', region_name = region['RegionName'])
 
   try:
-    print("Create defaut VPC in " + region['RegionName'])
+    print("\nCreate defaut VPC in " + region['RegionName'])
     client.create_default_vpc()
+    print("-> Created")
   except ClientError as error:
-    print("Error: " + error.response['Error']['Code'] + "(" + error.response['Error']['Message'] + ")")
+    if error.response['Error']['Code'] == 'InternalError':
+      print('-> Error Message: {}'.format(error.response['Error']['Message']))
+      print('-> Request ID: {}'.format(error.response['ResponseMetadata']['RequestId']))
+      print('-> Http code: {}'.format(error.response['ResponseMetadata']['HTTPStatusCode']))
+    elif error.response['Error']['Code'] == 'LimitExceededException':
+      print("-> Error: Limit exceed for VPC in region" + region['RegionName'])
+      print('-> Request ID: {}'.format(error.response['ResponseMetadata']['RequestId']))
+      print('-> Http code: {}'.format(error.response['ResponseMetadata']['HTTPStatusCode']))
+    elif error.response['Error']['Code'] == 'DefaultVpcAlreadyExists':
+      print("-> Default VPC in " + region['RegionName'] + " already exist. Skip.")
+    else:
+      print("-> Error: " + error.response['Error']['Code'] + " (" + error.response['Error']['Message'] + ")")
+      print('-> Request ID: {}'.format(error.response['ResponseMetadata']['RequestId']))
+      print('-> Http code: {}'.format(error.response['ResponseMetadata']['HTTPStatusCode']))
     pass
-  except:
-    print("Unknown error")
+  except ParamValidationError as error:
+    print('Unvalid parameters provided: {}'.format(error))
     pass
--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -4,13 +4,13 @@ aws-nuke $@
 
 if [ ! -z "$RECREATE_DEFAULT_VPC_RESOURCES" ]
 then
-  echo "RECREATE_DEFAULT_VPC_RESOURCES is set. This will create the default VPC, subnets, internet gateway, DHCP options and security groups after runnig aws-nuke."
-  
   is_dry_run=`echo "$@" |grep '\-\-no\-dry\-run'`
   
   if [ ! -z "$is_dry_run" ]
   then
+    echo "RECREATE_DEFAULT_VPC_RESOURCES is set. This will create the default VPC, subnets, internet gateway, DHCP options and security groups after runnig aws-nuke."
     create_aws_default_resources
   else
     echo "Skiping default VPC, subnets, internet gateway, DHCP options and security groups recreation in dry mode"
   fi
+fi
